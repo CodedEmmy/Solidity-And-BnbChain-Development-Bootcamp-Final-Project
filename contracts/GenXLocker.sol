97f04d8eb10e373178f27677b5f7422e07e0c936
CodedEmmy <95436001+CodedEmmy@users.noreply.github.com>
2023-10-30 13:25:16 +0100

    Add files via upload

--- contracts/GenXLocker.sol
+++ contracts/GenXLocker.sol
@@ -1,47 +1,53 @@
 // SPDX-License-Identifier: MIT 
+
 pragma solidity ^0.8.21;
 
 import "./Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-/** 
-@title A contract to handle the locking of tokens for a fixed duration with rewards
-@author Emmanuel Umoh
- */
+//A contract to handle the locking of tokens for a fixed duration with rewards
+// Author Emmanuel Umoh
+
 contract GenXLocker is Ownable
 {
     using SafeERC20 for IERC20;
 
-    //@notice This struct serves a record holder for a single staking transaction
-    struct LockerItem{
+    // This struct holds data related to a stake's reward.
+    struct Rewards {
         string lockedToken; //the ERC20 token being locked
         uint lockedAmount; //the amount of tokens locked
         uint startTime; //timestamp for the beginning of the stake
         uint endTime; //timestamp for the expiry of the stake
-        uint earnRate; //earning rate of staked tokens per second
+        bool lockState; // current state of the stake; true = active, false = expired
         uint stakeDuration; //lock duration in days
         uint computedRewards; //rewards calculated based on lock parameters
-        uint stakeAPY; //Annual expected yield, used to compute earn rate
-        bool lockState; // current state of the stake; true = active, false = expired
-        uint internalIndex; //index used to reference this record
     }
 
-    //@notice This struct represents a GenXLocker user and their stakes
-    struct LockerUser{
+    // This struct serves a record holder for a single staking transaction
+    struct LockerItem {
+        uint internalIndex; //index used to reference this record
+        Rewards itemReward; //rewards related variables
+        uint earnRate; //earning rate of staked tokens per second
+        uint stakeAPY; //Annual expected yield, used to compute earn rate
+    }
+
+    // This struct represents a GenXLocker user and their stakes
+    struct LockerUser {
         address userAddress;
         bool userExists; //flag to indicate if a user is a new or already existing user
         LockerItem[] stakes; //collection of the user's stake records
     }
 
-    //@notice A mapping of each supported stake token to their respective contract addresses
-    mapping(string => address) tokenContractMap;
+    // A mapping of each supported stake token to their respective contract addresses
+    mapping(string => address) private tokenContractMap;
 
-    //@notice A list of keys stored in the tokenContractMap
-    string[] tokenNameList;
+    // A list of keys stored in the tokenContractMap
+    string[] private tokenNameList;
     
-    //@notice A mapping of each user address to their corresponding stake records
-    mapping(address => LockerUser) stakersMap;
+    // A mapping of each user address to their corresponding stake records
+    mapping(address => LockerUser) private stakersMap;
 
     // emitted when a user makes a new stake
     event NewStake(address userAddress, uint amount);
@@ -55,9 +61,7 @@ contract GenXLocker is Ownable
         setupStakeTokens();
     }
 
-    /**
-    @notice populate the map of supported stake tokens. More tokens can later be added by the owner using the addStakeToken function
-     */
+    //Populate the map of supported stake tokens. More tokens can later be added by the owner using the addStakeToken function
     function setupStakeTokens() internal onlyOwner
     {
         //Test USDC
@@ -68,39 +72,26 @@ contract GenXLocker is Ownable
         tokenNameList.push("WUSD");
     }
 
-    /**
-    @notice Adds a new token to the list of tokens that can be staked
-    @param tokenName string name for the token
-    @param tokenCA the token's contract address
-     */
+    // Adds a new token to the list of tokens that can be staked
     function addStakeToken(string memory tokenName, address tokenCA) external onlyOwner
     {
         tokenContractMap[tokenName] = tokenCA;
         tokenNameList.push(tokenName);
     }
 
-    /**
-    @notice gets the list of supported tokens
-    @return string[] array containing the supported token names
-     */
-    function getStakeTokens() public view returns(string[] memory)
+    // Gets the list of supported tokens
+    function getStakeTokens() external view returns(string[] memory)
     {
         return tokenNameList;
     }
 
-    /**
-    @notice retrieves a user's record using their address
-    @param userAddr User's address
-    @return LockerUser object holding the stake records for the specified address
-     */
-    function getStaker(address userAddr) public view returns(LockerUser memory)
+    // Retrieves a user's record using their address
+    function getStaker(address userAddr) external view returns(LockerUser memory)
     {
         return stakersMap[userAddr];
     }
 
-    /**
-    * Internal function to add a new user to the contract
-     */
+    // Internal function to add a new user to the contract
     function addStaker(address userAddr) public pure returns(LockerUser memory)
     {
         LockerUser memory newUser;
@@ -110,112 +101,106 @@ contract GenXLocker is Ownable
         return newUser;
     }
 
-    /**
-    * This function adds a new stake transaction to the user's account
-    * @param stakeTokenName the name of the token to stake
-    * @param duration period of the lock (in days)
-    * @param amount number of tokens to be staked
-    * @param apy the APY
-    *
-    * Emits {NewStake} event
-     */
-    function newLock(string memory stakeTokenName, uint duration, uint amount, uint apy) public
+    // This function adds a new stake transaction to the user's account
+    // Emits {NewStake} event
+    
+    function newLock(string memory stakeTokenName, uint duration, uint amount, uint apy) external
     {
         //Use msg.sender as userAddr to make the transaction more secure since we want only the 
         //caller's wallet to be able to perform this operation
-        address userAddr = msg.sender;
-        LockerUser memory staker = stakersMap[userAddr];
+        LockerUser memory staker = stakersMap[msg.sender];
         if(!staker.userExists){
-            staker = addStaker(userAddr);
-            stakersMap[userAddr] = staker;
+            staker = addStaker(msg.sender);
+            stakersMap[msg.sender] = staker;
         }
-        IERC20 stakeToken = IERC20(tokenContractMap[stakeTokenName]);
-        stakeToken.safeTransferFrom(userAddr,address(this),amount);
+        doTransferFrom(stakeTokenName, amount, msg.sender);
+
         LockerItem memory stakeItem;
-        stakeItem.lockedToken = stakeTokenName;
-        stakeItem.lockedAmount = amount;
-        stakeItem.startTime = block.timestamp;
-        stakeItem.stakeDuration = duration;
-        uint durationInSec = duration * 86400; //changes duration from days to seconds
-        stakeItem.endTime = stakeItem.startTime + durationInSec;
+        stakeItem.itemReward.lockedToken = stakeTokenName;
+        stakeItem.itemReward.lockedAmount = amount;
+        stakeItem.itemReward.startTime = block.timestamp;
+        stakeItem.itemReward.stakeDuration = duration;
+        stakeItem.itemReward.endTime = getEndTime(duration, stakeItem.itemReward.startTime);
+        stakeItem.itemReward.lockState = true;
+        stakeItem.itemReward.computedRewards = 0;
+
         stakeItem.stakeAPY = apy;
         stakeItem.earnRate = computeRate(apy);
-        stakeItem.lockState = true;
-        stakeItem.computedRewards = 0;
         stakeItem.internalIndex = staker.stakes.length;
         staker.stakes[stakeItem.internalIndex] =  stakeItem;
 
-        emit NewStake(userAddr, amount);
+        emit NewStake(msg.sender, amount);
     }
 
-    /**
-    * Compute the earn rate in seconds from the annual rate
-     */
+    function getEndTime(uint duration, uint startTime) private pure returns(uint)
+    {
+        uint durationInSec = duration * 86400; //changes duration from days to seconds
+        return startTime + durationInSec;
+    }
+
+    // function to tranfer the specified amount from the given address to this contract address
+    function doTransferFrom(string memory tokenName, uint amt, address userAddr) private
+    {
+        IERC20 stakeToken = IERC20(tokenContractMap[tokenName]);
+        stakeToken.safeTransferFrom(userAddr,address(this),amt);
+    }
+
+    // function to transfer the specified amount from this contract to the given address
+    function doTransferTo(address userAddr, uint amt, string memory tokenName) private
+    {
+        IERC20 stakeToken = IERC20(tokenContractMap[tokenName]);
+        stakeToken.safeTransfer(userAddr, amt);
+    }
+
+    // Compute the earn rate in seconds from the annual rate
     function computeRate(uint theApy) private pure returns(uint)
     {
         uint rate = (theApy/100.0)/31536000; // divide apy by total number of seconds in a year to get the rate per second
         return rate;
     }
 
-    /**
-    * function to unlock a given stake
-    * @param sIndex index used to identify the particular stake record. Index is obtained from the internalIndex member of the LockerItem
-    *
-    * Emits a {UnlockStake} event
-     */
+    // Function to unlock a given stake
+    // Emits a {UnlockStake} event
     function unlockStake(uint sIndex) external
     {
-        address userAddr = msg.sender;
-        LockerUser memory staker = stakersMap[userAddr];
-        LockerItem memory stakeItem = staker.stakes[sIndex];
-        require(block.timestamp >= stakeItem.endTime,"Stake has not expired");
-        stakeItem.lockState = false;
-        stakeItem.computedRewards = getRewards(stakeItem);
-        IERC20 stakeToken = IERC20(tokenContractMap[stakeItem.lockedToken]);
-        uint unstakeAmount = stakeItem.lockedAmount + stakeItem.computedRewards;
-        stakeToken.safeTransfer(userAddr, unstakeAmount);
+        //LockerUser memory staker = stakersMap[msg.sender];
+        LockerItem memory stakeItem = stakersMap[msg.sender].stakes[sIndex];
+        require(block.timestamp >= stakeItem.itemReward.endTime,"Stake has not expired");
+        stakeItem.itemReward.lockState = false;
+        stakeItem.itemReward.computedRewards = getRewards(stakeItem);
+        uint unstakeAmount = stakeItem.itemReward.lockedAmount + stakeItem.itemReward.computedRewards;
+        doTransferTo(msg.sender, unstakeAmount, stakeItem.itemReward.lockedToken);
 
-        emit UnlockStake(userAddr, unstakeAmount);
+        emit UnlockStake(msg.sender, unstakeAmount);
     }
 
-    /**
-    * Internal function to compute the rewards earned by a user based on the stake parameters
-    * @param item LockerItem representing a particular staking record
-    * @return uint the computed reward amount
-     */
+    // Internal function to compute the rewards earned by a user based on the stake parameters
     function getRewards(LockerItem memory item) internal view returns(uint)
     {
         uint duration;
-        if(item.lockState){
-            duration = block.timestamp - item.startTime;
+        if(item.itemReward.lockState){
+            duration = block.timestamp - item.itemReward.startTime;
         }else{
-            duration = item.endTime - item.startTime;
+            duration = item.itemReward.endTime - item.itemReward.startTime;
         }
         if(duration < 0) duration = 0;
-        uint reward = item.lockedAmount * item.earnRate * duration;
+        uint reward = item.itemReward.lockedAmount * item.earnRate * duration;
         return reward;
     }
 
-    /**
-    * Function calculates the rewards for all stakes in the specified array
-    * @param li array of LockerItems containing stakes whose rewards we which to calculate
-    * @return LockerItem[] the array of LockerItems with computed rewards
-     */
-    function calculateRewards(LockerItem[] memory li) private view returns(LockerItem[] memory)
+    // Function calculates the rewards for all stakes in the specified array
+    function calculateRewards(LockerItem[] memory stakes) private view returns(Rewards[] memory)
     {
-        LockerItem[] memory stakes = li;
+        Rewards[] memory rewardList;
         for(uint i = 0;i < stakes.length;i++){
-            stakes[i].computedRewards = getRewards(stakes[i]);
+            stakes[i].itemReward.computedRewards = getRewards(stakes[i]);
+            rewardList[i] = stakes[i].itemReward;
         }
-        return stakes;
+        return rewardList;
     }
 
-    /**
-    * This function retrieves the collection of all stakes made by a specified user
-    * @param userAddr address of target user
-    * @return LockerItem an array of LockerItem objects
-     */
-    function getStakingData(address userAddr) public view returns(LockerItem[] memory)
+    // This function retrieves the collection of all stakes made by a specified user
+    function getStakingData(address userAddr) external view returns(Rewards[] memory)
     {
         LockerUser memory user = stakersMap[userAddr];
         return calculateRewards(user.stakes);
