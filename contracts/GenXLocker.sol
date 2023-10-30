86ec6ce0c79193fddb969c057e79a4c10ea78ee7
CodedEmmy <95436001+CodedEmmy@users.noreply.github.com>
2023-10-30 13:26:37 +0100

    Update GenXLocker.sol

--- contracts/GenXLocker.sol
+++ contracts/GenXLocker.sol
@@ -3,9 +3,9 @@
 pragma solidity ^0.8.21;
 
 import "./Ownable.sol";
-import "./openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 //A contract to handle the locking of tokens for a fixed duration with rewards
 // Author Emmanuel Umoh
